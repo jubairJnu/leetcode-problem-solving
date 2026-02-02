7818ba0570e3261461a3f37cbac15e6203a5922f
Abdullah Al Jubair <jubair2810@gmail.com>
2026-02-02 15:28:19 +0600

    aded day five

---
+++ day5.js
@@ -0,0 +1,35 @@
+// Example 1:
+
+nums = [1, 3, 5, 6];
+target = 7;
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
+
+console.log(searchInsert(nums, target));
